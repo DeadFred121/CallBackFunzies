1bc96d88adf17ef3703336134d7e2469369431ee
DeadFred <f.cliff1@hotmail.com>
2017-12-04 13:49:49 +1000

    New book created with callback function

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose.Promise = Promise;
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+mongoose.connect('mongodb://localhost/bookdb', {useMongoClient: true});
+
+const bookSchema = new Schema({
+  title: String,
+  author: String,
+  created_at: Date,
+  updated_at: Date
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+// create a new book
+const newBook = Book({
+  title: 'The King In Yellow',
+  author: 'Robert W. Chambers'
+});
+
+// save the user
+newBook.save(function (err) {
+  if (err) throw err;
+
+  console.log('Book created!');
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Book Depository listening on port 3000!'));
+
